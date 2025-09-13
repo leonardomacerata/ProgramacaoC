b3517e6ffbfaa38dc65a07b472493e67440fdc03
leonardomacerata <leonardomacerata@gmail.com>
2025-09-13 13:33:52 +0000

    ok

---
+++ operacoes_matematicas_atribuicao.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main(){
+    /*
+    Atribuição Simples (=)
+    Atribuição com Soma (+=)
+    Atribuição com Subtração (-=)
+    Atribuição com Multiplicação (*=)
+    Atribuição com Divisão (/=)
+    */
+
+    int numero1, numero2, resultado;
+    
+
+}
