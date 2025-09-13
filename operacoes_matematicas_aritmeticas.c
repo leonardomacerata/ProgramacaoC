5711ee67669e0dd5cdf76dcde79cabfd7c9a4e4a
leonardomacerata <leonardomacerata@gmail.com>
2025-09-13 13:31:19 +0000

    ok

---
+++ operacoes_matematicas_aritmeticas.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+
+int main(){
+    /*
+    Soma (+)
+    Subtração (-)
+    Multiplicação (*)
+    Divisão(/)
+    */
+
+    int numero1, numero2;
+    int soma, subtracao, multiplicacao, divisao;
+
+    printf("Digite o número 1: \n");
+    scanf("%d", &numero1);
+
+    printf("Digite o número 2: \n");
+    scanf("%d", &numero2);
+
+    //operação soma
+    soma = numero1 + numero2;
+
+    //operação subtração
+    subtracao = numero1 - numero2;
+
+    //operação multiplicação
+    multiplicacao = numero1 * numero2;
+
+    //operação divisão
+    divisao = numero1 / numero2;
+
+    printf("A soma é: %d \n", soma);
+    printf("A subtração é: %d \n", subtracao);
+    printf("A multiplicação é: %d \n", multiplicacao);
+    printf("A divisão é: %d \n", divisao);
+
+}
