64c439bd15b518413c00dd30eafe9fdc0b79f50a
leonardomacerata <leonardomacerata@gmail.com>
2025-09-13 17:07:55 +0000

    update

--- operacoes_matematicas_incrementoedecremento.c
+++ operacoes_matematicas_incrementoedecremento.c
@@ -9,3 +9,14 @@ int main(){
     Pré-Decremento --a
     Pós-Decremento a--
     */
+
+    int numero1 = 1;
+
+    printf("Antes do incremento: %d \n", numero1);
+
+    numero1++;
+
+    printf("Após o incremento: %d \n", numero1);
+
+
+}
