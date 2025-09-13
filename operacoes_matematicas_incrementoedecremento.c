be359a19e399a13ffbd9ced40e1032ae8195dbcc
leonardomacerata <leonardomacerata@gmail.com>
2025-09-13 17:00:13 +0000

    update

---
+++ operacoes_matematicas_incrementoedecremento.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    Incremento (++)
+    Pré-Incremento ++a
+    Pós-Incremento a++
+    Decremento (--)
+    Pré-Decremento --a
+    Pós-Decremento a--
+    */
