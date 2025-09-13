5b371b188ee4823b8e2caedda3bc6e2d69d28416
leonardomacerata <leonardomacerata@gmail.com>
2025-09-13 17:41:49 +0000

    update

--- operacoes_matematicas_incrementoedecremento.c
+++ operacoes_matematicas_incrementoedecremento.c
@@ -24,7 +24,13 @@ int main(){
     printf("Após o pós-incremento - Número 1: %d - Resultado: %d \n", numero1, resultado); //Pós incremento é a quantidade que foi adicionada ao resultado, neste caso 1
 
     resultado = ++numero1;
-    printf("Após o pré-incremento - Número 1: %d - Resultado: %d \n", numero1, resultado); //Pré incremento é o valor antes do incremento,
+    printf("Após o pré-incremento - Número 1: %d - Resultado: %d \n", numero1, resultado); //Pré incremento é o valor antes do incremento, neste caso 3
+
+    resultado = numero1--;
+    printf("Após o pós-decremento - Número 1: %d - Resultado: %d \n", numero1, resultado); //Pós decremento é a quantidade que foi subtraída do resultado, neste caso 2
+
+    resultado = --numero1;
+    printf("Após o pré-decremento - Número 1: %d - Resultado: %d \n", numero1, resultado); //Pré decremento é o valor antes do decremento, neste caso 3
 
 
 
