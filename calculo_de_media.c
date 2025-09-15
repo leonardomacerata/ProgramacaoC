76a25da922e77c49baeb272fc11a8fdd05895a3c
leonardomacerata <leonardomacerata@gmail.com>
2025-09-15 18:52:55 +0000

    update

--- calculo_de_media.c
+++ calculo_de_media.c
@@ -3,7 +3,15 @@
 int main(){
     int nota1, nota2, nota3, media;
 
-    printf("Cálculo de Média");
+    printf("Cálculo de Média \n");
+    printf("Digite a Nota 1: \n");
+    scanf("%d", &nota1);
+    printf("Digite a Nota 2: \n");
+    scanf("%d", &nota2);
+    printf("Digite a Nota 3: \n");
+    scanf("%d", &nota3);
+    media = (nota1 + nota2 + nota3)/3;
+    printf("Média: %d \n", media);
 
     
     
