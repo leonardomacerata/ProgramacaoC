06b79586fd48844e17f97e15d4c18fe4540868d5
leonardomacerata <leonardomacerata@gmail.com>
2025-09-15 17:58:51 +0000

    update

--- operacoes_matematicas_manipulacaoeconversao.c
+++ operacoes_matematicas_manipulacaoeconversao.c
@@ -4,9 +4,15 @@ int main (){
 
     int a = 10;
     float b = 3.5;
-    float resultado = a + b; // a variável a, que é do tipo int (inteira) é convertida implicitamente para uma variável resultante do tipo float (decimal)
+    float resultado = a + b; // a variável 'a', que é do tipo int (inteira), é convertida implicitamente para uma variável resultante do tipo float (decimal)
+
+    int c = 10;
+    float d = 3;
+    float quociente = (float) c / b; // a variável 'a', que é do tipo int (interira), é convertida explicitamente para a variável do tipo float (decimal)
 
     printf("Resultado = %.2f \n", resultado);
+
+    printf("Quociente = %.2f \n", quociente);
 
     return 0;
 
