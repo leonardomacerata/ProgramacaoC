45c3119c3bf3357a0200ffe210b9839c805be35c
leonardomacerata <leonardomacerata@gmail.com>
2025-09-15 18:16:03 +0000

    update

--- operacoes_matematicas_manipulacaoeconversao.c
+++ operacoes_matematicas_manipulacaoeconversao.c
@@ -4,15 +4,21 @@ int main (){
 
     int a = 10;
     float b = 3.5;
-    float resultado = a + b; // a variável 'a', que é do tipo int (inteira), é convertida implicitamente para uma variável resultante do tipo float (decimal)
+    float resultado = a + b; // a variável 'a', que é do tipo int (inteira), é convertida implicitamente para uma variável resultante do tipo float (decimal). Em algumas operações, como divisão, a conversão implítica pode não funciona. Veja exemplo abaixo
 
+    // conversão implícita as vezes não funciona na operação divisão
     int c = 10;
     float d = 3;
-    float quociente = (float) c / b; // a variável 'a', que é do tipo int (interira), é convertida explicitamente para a variável do tipo float (decimal)
+    float quociente1 = c / d; // INCORRETO: a variável 'c', que é do tipo int (inteira), é convertida implicitamente para a variável do tipo float (decimal). Note que o resultado é 3.00, onde deveria ser 3.33, conforme abaixo
+
+    // conversão explícita sempre funciona na operação divisão
+    float quociente2 = (float) c / d; // CORRETO: a variável 'c', que é do tipo int (inteira), é convertida explicitamente para a variável do tipo float (decimal)
 
     printf("Resultado = %.2f \n", resultado);
 
-    printf("Quociente = %.2f \n", quociente);
+    printf("Quociente com conversão implícita = %.2f \n", quociente1);
+
+    printf("Quociente com conversão explícita = %.2f \n", quociente2);
 
     return 0;
 
