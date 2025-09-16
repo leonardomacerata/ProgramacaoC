b9e9963bc65a9e5afe6f3569b5d02b10368b4b7d
leonardomacerata <leonardomacerata@gmail.com>
2025-09-16 17:10:13 +0000

    criação

---
+++ operacoes_matematicas_operadores_relacionais.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int a =10;
+    int b = 20;
+
+    //se verdadeiro = 1, se falso = 0
+    printf("a maior que b: %d \n", a > b);
+    printf("a menor que b b: %d \n", a < b);
+    printf("a igual b: %d \n", a == b);
+    printf("a diferente de b: %d \n", a != b);
+
+
+    return 0;
+}
