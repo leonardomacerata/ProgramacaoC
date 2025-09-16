ee4523abe10e17558142f25178f49125546257aa
leonardomacerata <leonardomacerata@gmail.com>
2025-09-16 20:21:31 +0000

    update

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,12 +6,13 @@ int main(){ //Inicia o programa
     float area1, pib1, densidadepopulacional1, pibpercapita1, superpoder1, area2, pib2, densidadepopulacional2, pibpercapita2, superpoder2; //Declaração das váriáveis do tipo float
     char estado1[50], codigodacarta1[3], nomedacidade1[50], estado2[50], codigodacarta2[3], nomedacidade2[50]; //Declaração das váriáveis do tipo char
 
-    printf("***** Bem vindo ao jogo Super Trunfo ***** \n");
-    printf("Instruções: \n");
+    printf("***** Bem-vindo ao jogo Super Trunfo ***** \n");
+    printf("\n"); //Espaço de 1 linha
+    printf("Instruções e Regras: \n");
     printf("1- Digite todos os dados para as duas cartas. \n");
-    printf("2- Veja os resultados das comparações dos dados. \n");
-    printf("3- Se o resultado for 1, então a Carta 1 vence. Se o resultado for 0, então a Carta 2 vence. \n");
-    printf("4- Exceção para o resultado: para o dado densidade populacional, se o resultado for 0, então a Carta 1 vence. Se o resultado for 1, então a Carta 2 vence. \n");
+    printf("2- Veja o resultado em Comparações das Cartas. \n");
+    printf("2.1- Se o resultado for 1, então a Carta 1 vence. Se o resultado for 0, então a Carta 2 vence. \n");
+    printf("2.2- Exceção para o resultado: para Densidade Populacional, se o resultado for 0, então a Carta 1 vence. Se o resultado for 1, então a Carta 2 vence. \n");
     printf("\n"); //Espaço de 1 linha
 
     //O usuário digita os dados para a carta 1
@@ -39,12 +40,12 @@ int main(){ //Inicia o programa
     printf("Nome da Cidade: %s \n", nomedacidade1); //Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
     printf("População: %u \n", populacao1); //Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
     printf("Área: %.2f km2 \n", area1); //Limita a variável a duas casa decimais. Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
-    printf("PIB: %.2f reais \n", pib1 ); //Limita a variável a duas casa decimais. Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
+    printf("PIB: R$%.2f \n", pib1 ); //Limita a variável a duas casa decimais. Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
     printf("Número de Pontos Turísticos: %d \n", pontosturisticos1 ); //Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
     densidadepopulacional1 = (float) populacao1 / area1; //calcula o resultado para a variável 'densidadepopulacional'. Converte a variável 'populacao' de int para float, e deixa o espaço para 1 linha
     printf("Densidade Populacional: %.2f hab/km2 \n", densidadepopulacional1); //Imprime o resultado da variável densidadepopulacional, limitando a 2 casas decimais, e deixa o espaço de 1 linha
     pibpercapita1 = pib1 / (float) populacao1; //calcula o resultado para a variável 'pibpercapita'. Converte a variável 'populacao' de int para float, e deixa o espaço para 1 linha
-    printf("PIB per Capita: %.2f reais \n", pibpercapita1); //Imprime o resultado da variável densidadepopulacional, limitando a 2 casas decimais, e deixa o espaço de 1 linha
+    printf("PIB per Capita: R$%.2f \n", pibpercapita1); //Imprime o resultado da variável densidadepopulacional, limitando a 2 casas decimais, e deixa o espaço de 1 linha
     superpoder1 = (float) populacao1 + area1 + pib1 + (float) pontosturisticos1 + pibpercapita1 + (1/densidadepopulacional1);
     printf("Super Poder: %f \n", superpoder1);
     printf("\n"); // Espaço após imprimir os dados
@@ -74,12 +75,12 @@ int main(){ //Inicia o programa
     printf("Nome da Cidade: %s \n", nomedacidade2); //Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
     printf("População: %u \n", populacao2); //Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
     printf("Área: %.2f km2 \n", area2); //Limita a variável a duas casa decimais. Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
-    printf("PIB: %.2f reais \n", pib2 ); //Limita a variável a duas casa decimais. Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
+    printf("PIB: R$%.2f \n", pib2 ); //Limita a variável a duas casa decimais. Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
     printf("Número de Pontos Turísticos: %d \n", pontosturisticos2 ); //Imprime o texto e os dados da variável que foi digitado pelo usuário, e deixa o espaço de 1 linha
     densidadepopulacional2 = (float) populacao2 / area2; //calcula o resultado para a variável 'densidadepopulacional'. Converte a variável 'populacao' de int para float, e deixa o espaço para 1 linha
     printf("Densidade Populacional: %.2f hab/km2 \n", densidadepopulacional2); //Imprime o resultado da variável densidadepopulacional, limitando a 2 casas decimais, e deixa o espaço de 1 linha
     pibpercapita2 = pib2 / (float) populacao2; //calcula o resultado para a variável 'pibpercapita'. Converte a variável 'populacao' de int para float, e deixa o espaço para 1 linha
-    printf("PIB per Capita: %.2f reais \n", pibpercapita2); //Imprime o resultado da variável densidadepopulacional, limitando a 2 casas decimais, e deixa o espaço de 1 linha
+    printf("PIB per Capita: R$%.2f \n", pibpercapita2); //Imprime o resultado da variável densidadepopulacional, limitando a 2 casas decimais, e deixa o espaço de 1 linha
     superpoder2 = (float) populacao2 + area2 + pib2 + (float) pontosturisticos2 + pibpercapita2 + (1/densidadepopulacional2);
     printf("Super Poder: %f \n", superpoder2);
 
