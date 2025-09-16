5e1b0f8e7f4e8755aa136e95223bbc5f26d33dc0
leonardomacerata <leonardomacerata@gmail.com>
2025-09-16 12:34:55 +0000

    update

--- operacoes_matematicas_modificadores.c
+++ operacoes_matematicas_modificadores.c
@@ -2,13 +2,13 @@
 
 int main(){
 
-    int numeronormal = 2147483647; // valor positivo máximo de int. Int tem valor negativo máximo de -2147483648. Tamanho máximo de int 4 bytes.
-    long int numerogrande = 2147483648; // duplica o valor máximo de int de 4 para 8 bytes.
-    long long int numeromuitogrande = 2147483648; // duplica o valor máximo de int de 4 para 8 bytes, igual ao long.
+    int numeronormal = 2147483647; // MAIOR valor de int = 2.147.483.647. MENOR valor de int = -2.147.483.648. Tamanho = 32 bits (4 bytes)
+    long int numerogrande = 9223372036854775807; // MAIOR valor de long int = 9.223.372.036.854.775.807. MENOR valor de long int = -9.223.372.036.854.775.808. Tamanho = 64 bits (8 bytes). Em alguns sistemas, long int tem 32 bits (4 bytes) igual a int.
+    long long int numeromuitogrande = 9223372036854775807; // Igual ao long int.
 
-    printf("Número normal (int): %d", numeronormal);
-    printf("Número grande (long int): %ld", numerogrande);
-    printf("Número normal (long long int): %lld", numeromuitogrande);
+    printf("Número normal (int): %d \n", numeronormal);
+    printf("Número grande (long int): %ld \n", numerogrande);
+    printf("Número muito grande (long long int): %lld \n", numeromuitogrande);
 
 
     return 0;
