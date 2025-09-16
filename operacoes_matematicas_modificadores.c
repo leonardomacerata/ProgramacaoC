027bd5c32750094082908d7df894a9caef76fe6f
leonardomacerata <leonardomacerata@gmail.com>
2025-09-16 13:30:53 +0000

    update

--- operacoes_matematicas_modificadores.c
+++ operacoes_matematicas_modificadores.c
@@ -5,9 +5,10 @@ int main(){
     int numeronormal = 2147483647; // MAIOR valor de int = 2.147.483.647. MENOR valor de int = -2.147.483.648. Tamanho = 32 bits (4 bytes)
     long int numerogrande = 9223372036854775807; // MAIOR valor de long int = 9.223.372.036.854.775.807. MENOR valor de long int = -9.223.372.036.854.775.808. Tamanho = 64 bits (8 bytes). Em alguns sistemas, long int tem 32 bits (4 bytes) igual a int.
     long long int numeromuitogrande = 9223372036854775807; // Igual ao long int, porém é 64 bits para qualquer sistema. Long pode ser 32 ou 64 bits dependendo do sistema.
-    float numeromenospreciso = 9.223372036854775807; // Números decimais com menor precisão do que double. Tamanho = 64 bits.
-    double numeromaispreciso = 9.223372036854775807; // Números decimais com maior precisão do que float. Tamanho = 64 bits.
-    long double numeromuitopreciso = 9.223372036854775807; // Números decimais com maior precisão do que double. Tamanho = 128 bits.
+    float numeromenospreciso = 9.223372036854775807; // Números decimais com menor precisão do que double. Máximo 9 dígitos significativos. Tamanho = 32 bits.
+    // NÃO EXISTE LONG FLOAT
+    double numeromaispreciso = 9.223372036854775807; // Números decimais com maior precisão do que float. Máximo 17 dígitos significativos. Tamanho = 64 bits.
+    long double numerograndemaispreciso = 9.223372036854775807; // Números decimais com maior precisão do que double. Máximo 19 dígitos significativos. Tamanho = 128 bits.
     
     unsigned int numeronormalpositivo = 2147483647; // unsigned aceita apenas números positivos.
 
@@ -16,9 +17,8 @@ int main(){
     printf("Número muito grande (long long int): %lld \n", numeromuitogrande);
     printf("Número decimal menos preciso (float): %f \n", numeromenospreciso);
     printf("Número decimal mais preciso (double): %.21f \n", numeromaispreciso);
-    printf("Número decimal muito preciso (double): %.21lf \n", numeromuitopreciso);
-
-    
+    printf("Número decimal grande mais preciso (long double): %.21lf \n", numerograndemaispreciso);
+    printf("\n");
     printf("Número normal positivo (int): %d \n", numeronormalpositivo);
 
     return 0;
