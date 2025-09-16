e9625493f6ab9406ccd98f05559822f393b38974
leonardomacerata <leonardomacerata@gmail.com>
2025-09-16 18:13:57 +0000

    criação

---
+++ gerenciamento_de_estoque.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+char produtoA[30] = "ProdutoA";
+char produtoB[30] = "ProdutoB";
+
+unsigned int estoqueA = 1000;
+unsigned int estoqueB = 2000;
+
+float valorA = 10.50;
+float valorB = 20.40;
+
+unsigned int estoqueMinimoA = 500;
+
+
+}
