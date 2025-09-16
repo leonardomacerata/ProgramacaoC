097bc40665af433bf9b46341b3abdc5f04214d5d
leonardomacerata <leonardomacerata@gmail.com>
2025-09-16 17:32:46 +0000

    update

--- operacoes_matematicas_operadores_relacionais.c
+++ operacoes_matematicas_operadores_relacionais.c
@@ -3,7 +3,7 @@
 int main(){
     int a =20;
     int b = 20;
-    float c = 20.0;
+    float c = 20.2;
     char d = 'a'; //char é caracter. Aqui nos referimos a char como um carcter da tabela ASCII, onde a = 97.
 
     //se verdadeiro = 1, se falso = 0
@@ -16,6 +16,8 @@ int main(){
     printf("a maior ou igual que c: %d \n", a >= c);
     printf("O valor ASCII de %c é: %d \n",d ,d);
     printf("char a maior ou igual que d: %d \n", a >= d);
+    printf("a maior ou igual que c: %d \n", a >= (int)c); //float é explicitamente tranformada em int
+
 
 
     return 0;
