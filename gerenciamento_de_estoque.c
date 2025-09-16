a8d005b9609193cb83a56d28c92062d223993d31
leonardomacerata <leonardomacerata@gmail.com>
2025-09-16 18:20:59 +0000

    update

--- gerenciamento_de_estoque.c
+++ gerenciamento_de_estoque.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main (){
+
 char produtoA[30] = "ProdutoA";
 char produtoB[30] = "ProdutoB";
 
@@ -11,6 +12,12 @@ float valorA = 10.50;
 float valorB = 20.40;
 
 unsigned int estoqueMinimoA = 500;
+unsigned int estoqueMinimoB = 2500;
 
+double valorTotalA;
+double valorTotalB;
 
+printf("Produto %s tem estoque %u e o valor unitário é %.2f \n", produtoA, estoqueA, valorA);
+
+return 0;
 }
