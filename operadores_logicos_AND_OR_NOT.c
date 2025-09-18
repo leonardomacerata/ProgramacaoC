77ab1bb60ad8b8932697b60e706e02a88a20ffcc
leonardomacerata <leonardomacerata@gmail.com>
2025-09-18 13:36:26 +0000

    update

--- operadores_logicos_AND_OR_NOT.c
+++ operadores_logicos_AND_OR_NOT.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    
+
+    int a = 10, b = 11;
+
+    if (a > 0 && b > 10){ // se a E b são maiores que x, então verdadeiro, se não falso
+        printf("Verdadeiro\n");
+    } else {
+        printf("Falso\n");
+    }
 
 
-    return 0,
+    return 0;
 }
