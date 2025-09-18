73ab5d9ea78dd6034f84a574c91fd76b5250defd
leonardomacerata <leonardomacerata@gmail.com>
2025-09-18 11:35:51 +0000

    update

--- verificar_se_o_numero_e_par.c
+++ verificar_se_o_numero_e_par.c
@@ -2,11 +2,15 @@
 
 int numero = 4, resultado;
 
-resultado = numero % 2; // % é um operador de divisão que verifica se resultado da operação. Retona 0 ou 1. 0 se a o resultado da divisão for inteiro. 1 de o resultado for decimal (tiver resto)
-
 int main (){
 
-    printf("O número é par: %d \n", resultado); // se o resultado for 0, então o número é par
+    resultado = numero % 2; // % é um operador de divisão que verifica se resultado da operação. Retona 0 ou 1. 0 se a o resultado da divisão for inteiro. 1 de o resultado for decimal (tiver resto)
+    
+    printf("O resultado é: %d \n", resultado);
+
+    if (resultado == 0){
+        printf("O número é par");
+    }
 
     return 0;
 
