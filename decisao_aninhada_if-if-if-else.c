aa927da7bd4f30653bda3d19d05ff89adffa4cc0
leonardomacerata <leonardomacerata@gmail.com>
2025-09-18 17:25:59 +0000

    update

--- decisao_aninhada_if-if-if-else.c
+++ decisao_aninhada_if-if-if-else.c
@@ -2,27 +2,38 @@
 
 int main(){
 
-    int = idade = 18, renda = 1000, dependentes = 2;
+    int idade =70, renda = 4000, dependentes = 2;
 
     //condição 1: idade entre 18 e 65 anos
     //condição 2: renda menor que 3000
     //condição 3: número de dependentes maior que 2
 
-    if (idade >= 18 && idade <= 65) {
-        //condição verdadeira
-        if (renda < 3000){
-            //condição verdadeira        
-        } else {
-            printf("Não entende ao critério de renda.\n");
+    if (idade >= 18 && idade <= 65)
+    {
+        if (renda < 3000)
+        {
+            if (dependentes > 2)
+            {
+                printf("Atende a todos os critérios.\n");
+            } else
+            {
+                printf("Não atende ao critério de dependentes.\n");
+            }
+            
+            
+        } else
+        {
+            printf("Não atende ao critério de renda.\n");
         }
-        if (dependentes > 2){
-            //condição verdadeira
-        } else {
-            printf("Não atende ao critério de dependentes.\n")
-        }
-    } else {
-        print("Não atende ao critário de idade.\n");
+        
+        
+    } else
+    {
+        printf("Não atende ao critéio de idade.\n");
     }
+    
+    
 
     return 0;
+
 }
