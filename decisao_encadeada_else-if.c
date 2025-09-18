b35ae3b227a1146158fd7b35789e60b179b62b3d
leonardomacerata <leonardomacerata@gmail.com>
2025-09-18 14:24:59 +0000

    update

--- decisao_encadeada_else-if.c
+++ decisao_encadeada_else-if.c
@@ -2,15 +2,18 @@
 
 int main(){
 
-    int idade = 59;
+    int idade = 17;
 
-    // Criança = menor que 12
-    //Adulto = maior que 12 e menor que 60
+    //Criança = menor que 12
+    //Adolescente = maior que 12 e menor que 18
+    //Adulto = maior que 18 e menor que 60
     //Idoso = maior que 60
 
     if (idade < 12){
         printf ("Criança.\n");
-    } else if (idade >= 12 && idade < 60) {
+    } else if (idade >= 12 && idade <18) {
+        printf("Adolescente.\n");
+    } else if (idade >= 18 && idade < 60) {
         printf("Adulto. \n");
     } else {
         printf("Idoso.\n");
