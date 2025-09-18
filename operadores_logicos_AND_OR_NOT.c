bd2aff28a2f4b3fc6c77f45ce3ca3181d54993d5
leonardomacerata <leonardomacerata@gmail.com>
2025-09-18 13:53:14 +0000

    update

--- operadores_logicos_AND_OR_NOT.c
+++ operadores_logicos_AND_OR_NOT.c
@@ -4,19 +4,19 @@ int main(){
 
     int a = -1, b = 9;
 
-    if (a > 0 && b > 10){ // AND: se a E b são maiores que x, então verdadeiro, se não falso
+    if (a > 0 && b > 10){ // AND (&&): se a E b são maiores que x, então verdadeiro, se não falso
         printf("Verdadeiro\n");
     } else {
         printf("Falso\n");
     }
 
-     if (a > 0 || b > 10){ // OR: se a OU b são maiores que x, então verdadeiro, se não falso
+     if (a > 0 || b > 10){ // OR (||): se a OU b são maiores que x, então verdadeiro, se não falso
         printf("Verdadeiro\n");
     } else {
         printf("Falso\n");
     }
 
-     if (!(a > 0)){ // NOT: inverte o resultado, ou seja, se a condição for verdadeiro, então o resultado será falso
+     if (!(a > 0)){ // NOT (!): inverte o resultado, ou seja, se a condição for verdadeiro, então o resultado será falso
         printf("Verdadeiro\n");
     } else {
         printf("Falso\n");
