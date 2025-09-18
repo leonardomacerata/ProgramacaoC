41357087a9ab379fc718e69a345af72b9720a208
leonardomacerata <leonardomacerata@gmail.com>
2025-09-18 18:14:16 +0000

    update

--- switch.c
+++ switch.c
@@ -3,7 +3,7 @@
 int main(){
     
     int opcao;
-    float saldo = 10.00;
+    float saldo = 10.00, deposito, saque;
 
     printf("Escolha a opção:\n");
     printf("1 - Saldo\n");
@@ -16,6 +16,18 @@ int main(){
     {
     case 1:
         printf("Saldo: R$%.2f \n", saldo);
+        break;
+
+    case 2:
+        printf("Digite o valor do depósito:\n");
+        scanf("%f", &deposito);
+        printf("Valor depositado: R$%.2f \n", deposito);
+        break;
+
+    case 3:
+        printf("Digite o valor do saque:\n");
+        scanf("%f", &saque);
+        printf("Valor sacado: R$%.2f \n", saque);
         break;
     
     default:
