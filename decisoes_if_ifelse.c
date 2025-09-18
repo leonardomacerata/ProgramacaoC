c526c145732de718730f4b239ce7850c1649c39b
leonardomacerata <leonardomacerata@gmail.com>
2025-09-18 11:16:21 +0000

    criação

---
+++ decisoes_if_ifelse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+float temperatura = 25.0;
+
+int main(){
+
+     
+        if (temperatura > 26.0){
+        
+        printf("Está calor.\n");
+        printf("Ligue o ventilador.\n");
+
+        //if else if (temperatura >= 35.0);
+        //printf("Está muito calor.");
+        //printf("Ligue o ar condicionado.");      
+    }
+
+    return 0;
+}
