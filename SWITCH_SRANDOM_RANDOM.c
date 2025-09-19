5c32b707c4182c3b063d73988a1fbb9428b3f98c
leonardomacerata <leonardomacerata@gmail.com>
2025-09-19 15:10:18 +0000

    update

--- SWITCH_SRANDOM_RANDOM.c
+++ SWITCH_SRANDOM_RANDOM.c
@@ -3,7 +3,35 @@
 #include <time.h>
 
 int main(){
-    
+
+    int opcao, numerosecret, palpite;
  
+    printf("Menu Principal\n");
+    printf("1 - Iniciar Jogo\n");
+    printf("2 - Ver Regras\n");
+    printf("3 - Sair do Jogo\n");
+    printf("Escolha uma Opção:\n");
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        /* code */
+        break;
+
+    case 2:
+        /* code */
+        break;
+
+    case 3:
+        /* code */
+        break;
+    
+    default:
+        printf("Opção Inválida\n");
+        break;
+    }
+
+
     return 0;
 }
