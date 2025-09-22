0707068357576a92f090417b2af8ec8082ebb129
leonardomacerata <leonardomacerata@gmail.com>
2025-09-22 18:30:42 +0000

    update

--- loop_de_reteticao_DO-WHILE.c
+++ loop_de_reteticao_DO-WHILE.c
@@ -4,7 +4,7 @@ int main(){
 
     int i = 1;
 
-    do
+    do  //garante a execução do primeiro comando. Casos de uso: menus e opções interativas com o usuário.
     {
         printf("%d\n", i);
         i++; 
