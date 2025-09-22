cc10ab2cd6fca6c1eed8e4cbc4bf3ab841753d5a
leonardomacerata <leonardomacerata@gmail.com>
2025-09-22 20:05:14 +0000

    update

--- loop_de_repeticao_exercicio1_FOR.c
+++ loop_de_repeticao_exercicio1_FOR.c
@@ -2,7 +2,17 @@
 
 int main(){
 
-    //Exercício: 
+    //Exercício: tabuada
 
+    int numero, i;
+
+    printf("Digite um número para o cálculo da tabuada:\n");
+    scanf("%d", &numero);
+
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%d x %d = %d \n", i, numero, i * numero);
+    }
+    
     return 0;
 }
