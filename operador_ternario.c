810a4053ff8389013e6394a0a8e4694474bff228
leonardomacerata <leonardomacerata@gmail.com>
2025-09-22 11:49:48 +0000

    update

--- operador_ternario.c
+++ operador_ternario.c
@@ -4,7 +4,7 @@ int main() {
   int idade = 20;
   char* resultado;
 
-  resultado = (idade >= 18) ? "Maior de idade" : "Menor de idade";  //É um IF escrito de forma compacta
+  resultado = (idade >= 18) ? "Maior de idade" : "Menor de idade";  //É um IF escrito de forma compacta. Fico mais bonito, porém só é possível executar apena 1 comando em If e 1 em Else
 
   printf("Resultado: %s\n", resultado);
 
