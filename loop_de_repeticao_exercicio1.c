6522bebdad63b746a4b945df56c13767ac3e59bd
leonardomacerata <leonardomacerata@gmail.com>
2025-09-22 19:01:04 +0000

    update

--- loop_de_repeticao_exercicio1.c
+++ loop_de_repeticao_exercicio1.c
@@ -2,11 +2,18 @@
 
 int main(){
 
+    //Exercício: imprimir apenas os número pares de 0 a 10
+
     int i = 0;
 
     while (i <= 10)
     {
-        
+        if (i % 2 == 0) //Todo número inteiro que divido por 2 tiver como resultado 0, é par. Se não, é ímpar. Comando para ímpar (i % 2 != 0).
+        {
+            printf("O número %d é par.\n", i);
+        }
+
+        i++;    
     }
     
     
