237de68459adc5f1ba51c7b0457846d383e6e66d
leonardomacerata <leonardomacerata@gmail.com>
2025-09-22 11:34:37 +0000

    update

--- operador_ternario.c
+++ operador_ternario.c
@@ -4,7 +4,7 @@ int main() {
   int idade = 20;
   char* resultado;
 
-  resultado = (idade >= 18) ? "Maior de idade" : "Menor de idade"; 
+  resultado = (idade >= 18) ? "Maior de idade" : "Menor de idade";  //É um IF escrito de forma compacta
 
   printf("Resultado: %s\n", resultado);
 
