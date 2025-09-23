b5b0958edc87a4e9b372c1d0689cca189311396d
leonardomacerata <leonardomacerata@gmail.com>
2025-09-23 12:02:03 +0000

    update

--- loops_aninhados.c
+++ loops_aninhados.c
@@ -2,6 +2,7 @@
 
 int main(){
 
+    // Loops aninhadas podem ser compostos por FOR, WHILE ou DO-WHILE. O mais utilizado é FOR.
     // Vamos imprimir a tabuada completa de 0 a 10
 
     for (int i = 1; i <= 10; i++) //loop externo. Só é executado após o loop interno completar o comando
