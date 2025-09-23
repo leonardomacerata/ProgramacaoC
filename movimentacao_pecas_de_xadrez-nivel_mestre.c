b8e0b3f30fd4a9cff63324ef7f88309c0c4b7d11
leonardomacerata <leonardomacerata@gmail.com>
2025-09-23 18:43:21 +0000

    update

--- movimentacao_pecas_de_xadrez-nivel_mestre.c
+++ movimentacao_pecas_de_xadrez-nivel_mestre.c
@@ -17,7 +17,18 @@ void moverBispo(int casas){
     }
 }
 
+void moverRainha(int casas){
+    if (casas > 0)
+    {
+        printf("Rainha se movimentou 1 casa para esquerda.\n");
+        moverRainha(casas - 1);
+    }
+
+}
+
 int main(){
+
+    int k = 1;
 
     printf("Movimentar a Torre 5 casas para a direita: \n");
     moverTorre(5);
@@ -27,6 +38,23 @@ int main(){
     printf("Movimentar o Bispo 5 casas na diagonal para cima e à direita: \n");
     moverBispo(5);
     
+    printf("\n\n");
+
+    printf("Movimentar a Rainha 8 casas para esquerda: \n");
+    moverRainha(8);
+
+    printf("\n\n");
+
+    printf("Movimentar o Cavalo para cima à direita: \n");
+    while (k--)
+    {
+        for (int l = 0; l < 2; l++)
+        {
+            printf("Cavalo se movimentou 1 casa para cima. \n");
+        }
+        printf("Cavalo se movimentou 1 casa para direita. \n");
+    }
+   
     printf("\n\n");
 
     return 0;
