1c3b7716984a06739e033760ff134922415b249b
leonardomacerata <leonardomacerata@gmail.com>
2025-09-23 12:03:13 +0000

    update

--- loop_de_repeticao_FOR.c
+++ loop_de_repeticao_FOR.c
@@ -2,6 +2,8 @@
 
 int main(){
 
+    // FOR é o loop mais utilizado.
+
     for (int i = 1; i <= 5; i++) // for (inicialização da variável; condição; incremento/decremento). É o loop mais utilizado. Ideal usar qdo vc sabe a quantidade de loops necessários.
     {
         printf("&d\n", i);
